ab1623fb21ae0abece944456b6893a5a2e1cc4b9
Jalen Roberts <jalenmr94@hotmail.com>
2019-07-11 16:41:49 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,41 @@
+function shout(string) {
+  return string.toUpperCase() 
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+    var lowercase = 'I can\'t hear you!'
+    lowercase.toLowerCase() === lowercase;
+    //true
+ 
+  var uppercase = 'YES INDEED!'
+  uppercase.toUpperCase() === uppercase;
+  // true
+ 
+  var mixedCase = 'I love you, too.'
+  mixedCase.toLowerCase() === mixedCase;
+  // false
+  mixedCase.toUpperCase() === mixedCase;
+  // false
+ 
+ if (lowercase) {
+   return lowercase;
+ }
+ else (uppercase) {return uppercase;
+   
+ }
+ else if (mixedCase) {return mixedCase;
+   
+ }
+}
